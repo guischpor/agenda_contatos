8c1c6a51945632272c473a89fbdc33c59e81dc91
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-02-14 19:02:59 -0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agenda de Contatos'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
