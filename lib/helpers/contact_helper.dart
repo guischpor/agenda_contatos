d6c113971cb5499ea01666ff6b83d7fac2bf8adb
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-02-14 19:44:38 -0200

    Update contact_helper.dart

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,1 +1,48 @@
+import 'package:sqflite/sqflite.dart';
 
+class ContactHelper {}
+
+//Definindo as colunas
+final String idColumn = 'idColumn';
+final String nameColumn = 'nameColumn';
+final String emailColumn = 'emailColumn';
+final String phoneColumn = 'phoneColumn';
+final String imgColumn = 'imgColumn';
+
+//Definindo as variaveis e transformado os contatos em mapa
+class Contact {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+//Construtor que pega o mapa e constroi o contato
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+//Funçao retorna os maps
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+//função que retorna a leitura dos contatos
+  @override
+  String toString() {
+    return 'Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)';
+  }
+}
