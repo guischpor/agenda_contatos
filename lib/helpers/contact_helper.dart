b484d843f920a56d56f508c4ba20528de6064133
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2019-02-14 20:18:03 -0200

    Update contact_helper.dart

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,13 +1,49 @@
+import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-
-class ContactHelper {}
+import 'package:async/async.dart';
 
 //Definindo as colunas
+final String contactTable = 'contactTable';
 final String idColumn = 'idColumn';
 final String nameColumn = 'nameColumn';
 final String emailColumn = 'emailColumn';
 final String phoneColumn = 'phoneColumn';
 final String imgColumn = 'imgColumn';
+
+//classe principal
+class ContactHelper {
+  //instanciado o database para todo o projeto
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+//buscando o data base
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+//iniciando o database, e instanciando o caminho
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'contacts.db');
+
+    //abrindo o banco de dados
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          'CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)');
+    });
+  }
+}
 
 //Definindo as variaveis e transformado os contatos em mapa
 class Contact {
